0a8b34e2fc9f45ea19f278a4be016ced6f8a1ccb
brunogarciacv <brunogarciacosta@gmail.com>
2018-08-22 16:04:53 -0300

    dbfix

--- index.js
+++ index.js
@@ -12,41 +12,54 @@ const bot = new TelegramBot(token, {polling: true});
 
 
 bot.onText(/\/adduser (.+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const resp = match[1];
- // Should add a OBJ to the parsed json that contains the chatid and push the match to an array of users
- 	if (checkUsername(match)) {
-		bot.sendMessage(chatId, `O usuário ${match} não está disponível`);
+	const chatId = msg.chat.id;
+	const resp = match[1];
+ // Should add an OBJ to the parsed json that contains the chatid and push the match to an array of users
+
+checkUsername(resp).then((data) => {
+ 	if (data) {
+		bot.sendMessage(chatId, `O usuário ${resp} não está disponível`);
 	} else {
-		bot.sendMessage(chatId, `O usuário ${match} está disponível`);
+		bot.sendMessage(chatId, `O usuário ${resp} está disponível`);
 	}
- 	addName (match, chatId)
+})
+ 	addName(resp, chatId)
 
 
 });
 
 function addName (name, chatId) {
-	db.users.push({
-	chatId: chatId,
-	username: name
 
+	const found = db.users.some(obj => obj["chatId"] === chatId);
+
+	if (!found) {
+		db.users.push({
+		chatId: chatId,
+		username: name
+		})
+		console.log("Not Found!");
+		fs.writeFile('data.json', JSON.stringify(db), 'utf-8', function(err) {
+		if (err) throw err
+		console.log("Done!")
 	})
+	} else { console.log("Found"); }
 }
+
 function checkUsername(username) {
 	const options = {
-  uri: `https://t.me/` + username,
-  transform: function (body) {
-    return cheerio.load(body);
-  }
+		uri: `https://t.me/${username}`,
+		transform: body => {
+		return cheerio.load(body);
+		}
 	};
-	rp(options)
-	.then(($) => {
-		let hasUser = $('.tgme_page_extra').length;
+
+	return rp(options).then(($) => {
+
+		const hasUser = $(".tgme_page_extra").length;
 		return hasUser;
 	})
-
 	.catch((err) => {
-
+		console.log(err);
 	});
 
 };
