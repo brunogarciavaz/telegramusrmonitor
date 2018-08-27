ed2e9408409bbe0ac370e6f635acab92ff1487a6
brunogarciacv <brunogarciacosta@gmail.com>
2018-08-26 22:46:33 -0300

    routine, remove, working version

--- index.js
+++ index.js
@@ -5,49 +5,61 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.ENVTOKEN;
 const rp = require('request-promise');
 const cheerio = require('cheerio');
-
-
-
 const bot = new TelegramBot(token, {polling: true});
 
-
-bot.onText(/\/adduser (.+)/, (msg, match) => {
+bot.onText(/\/add (.+)/, (msg, match) => {
 	const chatId = msg.chat.id;
-	const resp = match[1];
- // Should add an OBJ to the parsed json that contains the chatid and push the match to an array of users
+	const username = match[1].replace('@','');;
+	checkUsername(username).then((hasUser) => {
+		if (hasUser) {
+			addName(username, chatId);
+			bot.sendMessage(chatId, `@${username} is now being monitored`);
+		} else {
+			bot.sendMessage(chatId, `@${username} is already available, you can only monitor usernames that are not available.`);
+		}
+	})
+});
 
-checkUsername(resp).then((data) => {
- 	if (data) {
-		bot.sendMessage(chatId, `O usuário ${resp} não está disponível`);
-	} else {
-		bot.sendMessage(chatId, `O usuário ${resp} está disponível`);
-	}
-})
- 	addName(resp, chatId)
-
-
+bot.onText(/\/remove (.+)/, (msg, match) => {
+	const chatId = msg.chat.id;
+	const username = match[1].replace('@','');;
+	removeName(username, chatId);
 });
 
 function addName (name, chatId) {
+	const userObj = db.users.find(obj => obj.chatId === chatId);
 
-	const found = db.users.some(obj => obj["chatId"] === chatId);
-
-	if (!found) {
+	if (userObj === undefined) {
 		db.users.push({
 		chatId: chatId,
-		username: name
+		username: [name]
 		})
 		console.log("Not Found!");
-		fs.writeFile('data.json', JSON.stringify(db), 'utf-8', function(err) {
-		if (err) throw err
-		console.log("Done!")
-	})
-	} else { console.log("Found"); }
+		fs.writeFile('data.json', JSON.stringify(db), 'utf-8', error => { if (error) throw error })
+	} else if (!userObj.username.includes(name)) {
+		userObj.username.push(name);
+		fs.writeFile('data.json', JSON.stringify(db), 'utf-8', error => { if (error) throw error })
+		console.log("Found, pushed")
+	}
+}
+
+function removeName(username, chatId) {
+	const userObj = db.users.find(obj => obj.chatId === chatId);
+
+	if (!userObj === undefined && userObj.username.includes(username)) {
+		 userObj.username.splice(userObj.username.indexOf(username), 1);
+		 fs.writeFile('data.json', JSON.stringify(db), 'utf-8', error => { if (error) throw error })
+		 bot.sendMessage(chatId, `@${username} is no longer being monitored`);
+
+	} else {
+		 bot.sendMessage(chatId, `This user is not currently being monitored, use /add @${username} to start monitoring this username`);
+	}
+
 }
 
 function checkUsername(username) {
 	const options = {
-		uri: `https://t.me/${username}`,
+		uri: `https://t.me/` + username,
 		transform: body => {
 		return cheerio.load(body);
 		}
@@ -63,3 +75,25 @@ function checkUsername(username) {
 	});
 
 };
+
+
+function routine() {
+	db.users.forEach(userObj => {
+	userObj.username.forEach(username => {
+		checkUsername(username).then(hasUser => {
+		if(!hasUser) {
+			bot.sendMessage(userObj.chatId, `‼️ Username @${username} is now AVAILABLE ‼️`);
+			removeName(username, userObj.chatId);
+
+		};
+
+		})
+	})
+});
+}
+
+setInterval(routine, 1800000);
+
+bot.on('polling_error', (error) => {
+	console.log(error.code);  // => 'EFATAL'
+});
